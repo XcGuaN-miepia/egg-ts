edddf9ed98cfe5af6b4e888478ac5354c5f48b1b
咩屁啊 <zhou.zhangtao@yalla.live>
2021-05-25 17:27:30 +0800

    fixed: 缺少await导致程序运行错误问题

--- app/controller/test.ts
+++ app/controller/test.ts
@@ -4,9 +4,8 @@ export default class TestController extends Controller {
     const { ctx } = this;
 
     // 假装这是从数据库读取的数据
-    const user = this.service.test.getData();
+    const user = await this.service.test.getData();
 
-    console.log(ctx.model);
     ctx.body = {
       code: 0,
       message: 'success',
